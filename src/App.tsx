91d1ce909d47d4c88f10bb4a897a7811f2638f66
ck837 <1264504656@qq.com>
2024-12-17 23:33:40 +0800

    feat: 修改项目

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,3 @@ function App() {
 }
 
 export default App
-
-// import React from 'react';
-// import ThreeScene from './ThreeScene';
-
-// const App: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Three.js with React</h1>
-//       <ThreeScene />
-//     </div>
-//   );
-// };
-
-// export default App;
